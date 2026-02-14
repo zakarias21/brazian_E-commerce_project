a2f803034166f985d4d391277e8dfd0561896901
zakarias21 <zakariasmusa@gmail.com>
2026-02-15 00:52:49 +0300

    some comments added

--- Data_Exploration_&_Validation.sql
+++ Data_Exploration_&_Validation.sql
@@ -7,13 +7,17 @@ UNION ALL SELECT 'orders', COUNT(*) FROM olist_orders_dataset
 UNION ALL SELECT 'products', COUNT(*) FROM olist_products_dataset
 UNION ALL SELECT 'sellers', COUNT(*) FROM olist_sellers_dataset
 UNION ALL SELECT 'category_translation', COUNT(*) FROM product_category_name_translation;
+--* see which tables are large/small.
+--* Helps anticipate performance considerations.
+----------------------------------------------------------------------------------------------------------------
 -- Check missing order dates
 SELECT 
     SUM(CASE WHEN order_purchase_timestamp IS NULL THEN 1 ELSE 0 END) AS missing_purchase_dates,
     SUM(CASE WHEN order_delivered_customer_date IS NULL THEN 1 ELSE 0 END) AS missing_delivery_dates,
     COUNT(*) AS total_orders
 FROM olist_orders_dataset;
-
+--* Delivery analysis relies on these timestamps
+----------------------------------------------------------------------------------------------------------------
 -- uniqueness checking codes
 SELECT order_id, COUNT(*) AS cnt
 FROM olist_orders_dataset
